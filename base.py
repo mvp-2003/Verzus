3e8483bc70cf4bdf0f9e0ca0a135362648ff0c0c
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-01-21 19:53:05 +0000

    Made too many changes to describe

--- base.py
+++ base.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.common.by import By
 
-serv = ChromeService("")
+serv = ChromeService("chromedriver.exe")
 ch = webdriver.Chrome(service=serv)
