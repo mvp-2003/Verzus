c2e7618764f64fd7f2af8680652b4f84c10a0a4e
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-01-21 19:33:41 +0000

    Added the webdriver and initialized the driver file

---
+++ back/base.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+
+serv = ChromeService("")
+ch = webdriver.Chrome(service=serv)
